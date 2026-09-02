78008a35ece938eced09f8a89b12955ad68fc001
v4r6whmsft-dev <v4r6whmsft@privaterelay.appleid.com>
2026-09-02 16:35:44 +0800

    Add Chinese drug knowledge seed

---
+++ drug_knowledge_seed.js
@@ -0,0 +1,12 @@
+window.DRUG_KNOWLEDGE_SEED=[
+{drugName:'贝伐珠单抗注射液',specText:'100mg',price:1180,concentration:'10 mg/ml',solvent:'0.9% NaCl 100 mL',reconstitutionVolumeText:'10ml',shareable:'可共享'},
+{drugName:'注射用曲妥珠单抗',specText:'150mg',price:3600,concentration:'21 mg/ml',solvent:'0.9% NaCl 250 mL',reconstitutionVolumeText:'7.2ml',shareable:'可共享'},
+{drugName:'注射用盐酸吉西他滨',specText:'1000mg',price:680,concentration:'40 mg/ml',solvent:'0.9% NaCl 100 mL',reconstitutionVolumeText:'25ml',shareable:'可共享'},
+{drugName:'注射用培美曲塞二钠',specText:'500mg',price:1460,concentration:'25 mg/ml',solvent:'0.9% NaCl 100 mL',reconstitutionVolumeText:'20ml',shareable:'可共享'},
+{drugName:'注射用奥沙利铂',specText:'100mg',price:720,concentration:'5 mg/ml',solvent:'5% Glucose 250 mL',reconstitutionVolumeText:'20ml',shareable:'可共享'},
+{drugName:'利妥昔单抗注射液',specText:'100mg',price:1200,concentration:'10 mg/ml',solvent:'0.9% NaCl 500 mL',reconstitutionVolumeText:'10ml',shareable:'可共享'},
+{drugName:'紫杉醇注射液',specText:'30mg',price:230,concentration:'6 mg/ml',solvent:'0.9% NaCl 250 mL',reconstitutionVolumeText:'5ml',shareable:'可共享'},
+{drugName:'卡铂注射液',specText:'100mg',price:200,concentration:'10 mg/ml',solvent:'5% Glucose 250 mL',reconstitutionVolumeText:'10ml',shareable:'可共享'},
+{drugName:'注射用亮丙瑞林微球',specText:'11.25mg',price:1800,concentration:'11.25 mg/ml',solvent:'专用溶媒 1 mL',reconstitutionVolumeText:'1ml',shareable:'不共享，皮下注射，一般整支使用'},
+{drugName:'临床试验盲法药',specText:'80mg',price:0,concentration:'8 mg/ml',solvent:'专用溶媒 50 mL',reconstitutionVolumeText:'10ml',shareable:'不共享'}
+];
