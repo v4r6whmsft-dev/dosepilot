50fa9df673a579d26a0bf269995c4639ed7cbf29
v4r6whmsft-dev <v4r6whmsft@privaterelay.appleid.com>
2026-09-02 16:36:06 +0800

    Add English drug knowledge seed

---
+++ drug_knowledge_seed_en.js
@@ -0,0 +1,12 @@
+window.DRUG_KNOWLEDGE_SEED=[
+{drugName:'Bevacizumab Injection',specText:'100mg',price:1180,concentration:'10 mg/ml',solvent:'0.9% NaCl 100 mL',reconstitutionVolumeText:'10ml',shareable:'Shareable'},
+{drugName:'Trastuzumab for Injection',specText:'150mg',price:3600,concentration:'21 mg/ml',solvent:'0.9% NaCl 250 mL',reconstitutionVolumeText:'7.2ml',shareable:'Shareable'},
+{drugName:'Gemcitabine Hydrochloride for Injection',specText:'1000mg',price:680,concentration:'40 mg/ml',solvent:'0.9% NaCl 100 mL',reconstitutionVolumeText:'25ml',shareable:'Shareable'},
+{drugName:'Pemetrexed Disodium for Injection',specText:'500mg',price:1460,concentration:'25 mg/ml',solvent:'0.9% NaCl 100 mL',reconstitutionVolumeText:'20ml',shareable:'Shareable'},
+{drugName:'Oxaliplatin for Injection',specText:'100mg',price:720,concentration:'5 mg/ml',solvent:'5% Glucose 250 mL',reconstitutionVolumeText:'20ml',shareable:'Shareable'},
+{drugName:'Rituximab Injection',specText:'100mg',price:1200,concentration:'10 mg/ml',solvent:'0.9% NaCl 500 mL',reconstitutionVolumeText:'10ml',shareable:'Shareable'},
+{drugName:'Paclitaxel Injection',specText:'30mg',price:230,concentration:'6 mg/ml',solvent:'0.9% NaCl 250 mL',reconstitutionVolumeText:'5ml',shareable:'Shareable'},
+{drugName:'Carboplatin Injection',specText:'100mg',price:200,concentration:'10 mg/ml',solvent:'5% Glucose 250 mL',reconstitutionVolumeText:'10ml',shareable:'Shareable'},
+{drugName:'Leuprorelin Microspheres for Injection',specText:'11.25mg',price:1800,concentration:'11.25 mg/ml',solvent:'Dedicated solvent 1 mL',reconstitutionVolumeText:'1ml',shareable:'Not shareable, subcutaneous injection, usually full-unit use'},
+{drugName:'Blinded Clinical Trial Drug',specText:'80mg',price:0,concentration:'8 mg/ml',solvent:'Dedicated solvent 50 mL',reconstitutionVolumeText:'10ml',shareable:'Not shareable'}
+];
